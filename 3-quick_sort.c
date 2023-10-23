14bbdc174599a64dfdc0b8dc7425bb77c32fb8d2
Kirito-YO <ussf.lord@gmail>
2023-10-23 16:14:29 -0700

    all last task

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,65 +1,65 @@
 #include "sort.h"
 /**
- * partition_array - function that do a partitions array
- * @array: VAR1
- * @l: VAR2
- * @r: VAR3
- * @n: VAR4
- * Return: INT.
+ * partition - partitions array
+ * @array: the array
+ * @l: left index
+ * @r: right index
+ * @n: size of array
+ * Return: int
  */
-int partition_array(int *array, int l, int r, size_t n)
+int partition(int *array, int l, int r, size_t n)
 {
-	int t, p, a, f;
+	int temp, pivot, i, j;
 
-	p = array[r];
-	a = l - 1;
+	pivot = array[r];
+	i = l - 1;
 
-	for (f = l; f <= r - 1; f++)
+	for (j = l; j <= r - 1; j++)
 	{
-		if (p > array[f])
+		if (pivot > array[j])
 		{
-			a++;
-			t = array[a];
-			array[a] = array[f];
-			array[f] = t;
+			i++;
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
 		}
 	}
 	print_array(array, n);
-	t = array[a + 1];
-	array[a + 1] = p;
-	array[r] = t;
+	temp = array[i + 1];
+	array[i + 1] = pivot;
+	array[r] = temp;
 
-	return (a + 1);
+	return (i + 1);
 }
 /**
- * sort_rec_array - function that recursively sorts array
- * @array: var1
- * @l: VAR2
- * @r: VAR3
- * @size: var4
- * Return: Null
+ * sort_array - recursively sorts array
+ * @array: integer array
+ * @l: left index
+ * @r: right index
+ * @size: size of array
+ * Return: void
  */
-void sort_rec_array(int *array, int l, int r, size_t size)
+void sort_array(int *array, int l, int r, size_t size)
 {
-	int a;
+	int p;
 
 	if (l < r)
 	{
-		a = partition_array(array, l, r, size);
-		sort_rec_array(array, l, a - 1, size);
-		sort_rec_array(array, a + 1, r, size);
+		p = partition(array, l, r, size);
+		sort_array(array, l, p - 1, size);
+		sort_array(array, p + 1, r, size);
 	}
 }
 /**
- * quick_sort - fucntion that :sorts array of integers using quick
- * @array: VAR1
- * @size: VAR2
- * Return: null.
+ * quick_sort - sorts array of integers using quick sort algorithm
+ * @array: array of integers
+ * @size: size of array
+ * Return: void
  */
 void quick_sort(int *array, size_t size)
 {
 	if (array)
 	{
-		sort_rec_array(array, 0, size - 1, size);
+		sort_array(array, 0, size - 1, size);
 	}
 }
