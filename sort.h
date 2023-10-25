e87efc2881ea9de2476e7496303872a4d33318de
aouraghse <aouraghse@gmail.com>
2023-10-24 17:52:24 -0700

    task 101

--- sort.h
+++ sort.h
@@ -22,6 +22,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 void quick_sort(int *array, size_t size);
 int sp(int *arr, int left, int right, size_t size);
 
